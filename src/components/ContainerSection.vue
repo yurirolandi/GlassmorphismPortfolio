c200c145b8c2255d5b4a7ee10995ee29fc87a345
yuri <sd.rolandi@gmail.com>
2022-01-21 21:48:23 -0300

    feat: create template container

---
+++ src/components/ContainerSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-section">
+    <div class="container">
+      <div class="row">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "container-section",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/variables.scss";
+
+.container-section {
+  background-color: $white-alpha-25;
+  border: 1px solid $white-alpha-40;
+  min-height: calc(100vh - 70px);
+  border-radius: 30px;
+  padding: 0 40px;
+  width: 100%;
+  align-items: center;
+  display: flex;
+  .container {
+    padding: 0 40px;
+    width: 100%;
+    .row {
+      display: flex;
+      flex-wrap: wrap;
+    }
+  }
+}
+</style>
