fb3f126f26c065d141839522720f0b4b841766ef
yuri <sd.rolandi@gmail.com>
2022-01-21 21:48:47 -0300

    feat: create template skills

---
+++ src/components/Skills.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="skills">
+    <div class="skills-item" v-for="(skill, i) in skillItems" :key="i">
+      {{ skill }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  props: {
+    skillItems: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+.skills {
+  display: flex;
+  flex-wrap: wrap;
+  &-item {
+    background: $white-alpha-25;
+    border: 1px solid $white-alpha-40;
+    padding: 5px 15px;
+    text-transform: capitalize;
+    margin: 0 10px 10px 0;
+    border-radius: 20px;
+  }
+}
+</style>
