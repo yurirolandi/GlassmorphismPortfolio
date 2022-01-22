2233be034e8a7117a2732a961b4271ce1e794ba7
yuri <sd.rolandi@gmail.com>
2022-01-21 21:50:32 -0300

    feat: create view home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home-section">
+    <ContainerSection>
+      <div class="home-text">
+        <p>Hello, I'm</p>
+        <h1>Yuri Rolandi</h1>
+        <h2>Frontend developer</h2>
+        <Btn text="More about me" />
+        <Btn text="portfolio" />
+      </div>
+      <div class="home-img">
+        <div class="home-box">
+          <img src="../assets/perfil-sem-fundo.png" alt="Yuri Rolandi" />
+        </div>
+      </div>
+    </ContainerSection>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import ContainerSection from "@/components/ContainerSection.vue";
+import Btn from "@/components/Button.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    ContainerSection,
+    Btn,
   },
 };
 </script>
+
+<style scoped lang="scss">
+@import "../styles/variables.scss";
+
+.home-section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .home-text {
+    > p {
+      font-size: 18px;
+    }
+    > h1 {
+      font-size: 50px;
+      text-transform: capitalize;
+    }
+    > h2 {
+      font-size: 20px;
+      text-transform: capitalize;
+      font-weight: 300;
+      margin-bottom: 30px;
+    }
+  }
+  .home-img {
+    .home-box {
+      max-width: 360px;
+      background-color: $white-alpha-25;
+      border-radius: 50%;
+      border: 8px solid $white-alpha-25;
+      margin: auto;
+      overflow: hidden;
+      height: 355px;
+      img {
+        width: 100%;
+        margin: auto;
+      }
+    }
+  }
+  .home-img,
+  .home-text {
+    width: 50%;
+    padding: 15px;
+  }
+}
+</style>
